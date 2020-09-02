756274d6c577d71e7186b6514d60add9a90cae96
Erik Weir <erik.weir@key.me>
2020-09-02 19:47:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,7 @@
-nums = [25, 7, 1]
-
-def sort_array_asc(nums)
-  nums.sort do |a, b|
-  if a == b
-    0
-  elsif a < b
-    -1
-  elsif a > b
-    1
-  end
+def sort_array_asc(array)
+  array.sort
 end
+
+def sort_array_asc(array)
+  array.sort {|x,y| y <=> x }
 end
